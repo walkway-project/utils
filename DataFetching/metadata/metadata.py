df1c35d762b0e3b6089856ce3fd0104abbbf9ca8
Ethan Havemann <echavemann@gmail.com>
2023-09-28 20:08:36 -0500

    Refactoring Download Metadata

--- DataFetching/metadata/metadata.py
+++ DataFetching/metadata/metadata.py
@@ -6,34 +6,39 @@ class MetadataGenerator:
 
     def __init__(self):
         self.metadata = {} #schema: type -> dict containing specific data
+        self.features = set() 
         self.symbols = set() 
-        self.DATAKEY = "data"
-        self.FEATUREKEY = "features"
-        self.metadata[self.DATAKEY] = {}
-        self.metadata[self.FEATUREKEY] = {}
+
+
+        self.FEATURE_KEY = "features"
+        self.SYMBOL_KEY = "symbols"
+        self.TIMEFRAME_KEY = "timeframe"
+        self.OHLCV_FNAME = "ohlcv"
+        self.ORDERBOOK_FNAME = "orderbook"
+
+        self.downloaded = False
+
+    #we are going to count everything as features.
 
     def generate_download_metadata(self, symbols, timeframe:TimeFrame):
         """
-        Stores metadata for symbols + timeframe.
+        ONLY called after downloading. Sets symbols, timeframe, and features. 
         """
-        data = self.metadata[self.DATAKEY]
-        data["symbols"] = symbols
+        if self.downloaded:
+            raise Exception("Can only generate download metadata once!")
         self.symbols = set(symbols)
-        data["timeframe"] = timeframe
+        self.features = set([self.OHLCV_FNAME, self.ORDERBOOK_FNAME])
+        self.metadata[self.SYMBOL_KEY] = symbols
+        self.metadata[self.TIMEFRAME_KEY] = timeframe
+        self.downloaded = True
 
-    def generate_feature_metadata(self, features, timeframe:TimeFrame, symbols = []):
+
+
+    def generate_feature_metadata(self, features:set):
         """
-        Stores metadata for all features and their timeframes for given symbols. If the download metadata
-        has already been generated, this will use the same symbols.
+        Stores metadata for all features and their timeframes for symbols.
         """
-        if not (self.symbols or symbols):
-            raise Exception("Symbols must be defined")
-        data = self.metadata[self.FEATUREKEY]
-        data[features] = features
-        if not self.symbols:
-            self.metadata[self.DATAKEY]["symbols"] = symbols
-            self.symbols = set(symbols)
-        data[timeframe] = timeframe
+        self.metadata[features] = self.metadatafeatures.union(features)
 
     def set_metadata(self, metadata):
         """
@@ -41,23 +46,21 @@ class MetadataGenerator:
         """
         self.metadata = metadata
         try:
-            self.symbols = set(metadata[self.DATAKEY]["symbols"])
+            self.symbols = set(metadata[self.SYMBOL_KEY])
         except:
-            raise Exception("Metadata is malformed!")
+            raise Exception("Could not parse the provided metadata!")
 
-    def read_metadata(self, path, type):
+    def read_metadata(self, path, callback = False):
         """
         Extracts metadata from Path. You can supply spcific types or "all".
         """
-        if (type not in self.metadata.keys() and type != "all"):
-            raise Exception("Metadata type is not supported!")
         with open(path.joinpath("metadata.json"), "r") as json_file:
             data = json_file.read()
         mdDict = json.loads(data)
-        if (type == "all"):
+        if callback:
             return mdDict
         else:
-            return mdDict[type]
+            self.metadata = mdDict
 
 
     def author_metadata(self, path):
