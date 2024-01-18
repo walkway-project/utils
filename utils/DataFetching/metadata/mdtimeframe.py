4e6aa66ba3a079d3b66dac7d38de141311915619
Ethan Havemann <echavemann@gmail.com>
2024-01-17 21:23:59 -0600

    Adding RECENT timeframe

--- utils/DataFetching/metadata/mdtimeframe.py
+++ utils/DataFetching/metadata/mdtimeframe.py
@@ -1,6 +1,7 @@
 from enum import Enum
 class TimeFrame(Enum):
     TEST = -1 #6/1/2023 to 7/1/2023
+    RECENT = 0 # 9/1/2023 to 1/15/2024
     SHORT = 1 #1/1/2022 to 7/1/2023
     MED = 2 #1/1/2020 to 7/1/2023
     LONG = 3 #1/1/2017 to 7/1/2023
