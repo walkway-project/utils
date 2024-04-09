52fd67fba04d33b0db7535f024023543d5d00049
Jiale Chen <55820495+jiale0402@users.noreply.github.com>
2024-04-08 22:49:07 -0500

    Update mdtimeframe.py

--- utils/DataFetching/metadata/mdtimeframe.py
+++ utils/DataFetching/metadata/mdtimeframe.py
@@ -4,7 +4,8 @@ from enum import Enum, unique
 @unique
 class TimeFrame(Enum):
     TEST = -1  # 6/1/2023 to 7/1/2023
-    RECENT = 0  # 9/1/2023 to 1/15/2024
+    RECENT = 0  # 9/1/2023 to 4/1/2024
     SHORT = 1  # 1/1/2022 to 7/1/2023
     MED = 2  # 1/1/2020 to 7/1/2023
     LONG = 3  # 1/1/2017 to 7/1/2023
+    
