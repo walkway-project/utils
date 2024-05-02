b90a4ae6736eaf4f587e26e7e3fb0d7f784ab34d
Ethan Havemann <echavemann@gmail.com>
2024-05-01 20:56:18 -0500

    Major rebuild of data fetching code

--- utils/DataFetching/download.py
+++ utils/DataFetching/download.py
@@ -2,7 +2,7 @@ from catalyst.utils.utils.DataFetching.helpers.downloader import download
 from catalyst.utils.utils.DataFetching.helpers.parser import process_download 
 from catalyst.utils.utils.DataFetching.metadata.metadata import MetadataGenerator
 from catalyst.utils.utils.DataFetching.metadata.mdtimeframe import TimeFrame
-from catalyst.grail.grail.modules.PrimitiveFeatureCacher import PrimitiveFeatureCacher
+from catalyst.utils.utils.DataFetching.markets import Market
 from catalyst.constants import DEFAULTPATH
 
 import os
@@ -12,59 +12,34 @@ class PathWarning(UserWarning): pass
 
 metadataGenerator = MetadataGenerator()
 
-def initializeSymbol(symbol, catalystBase, timeFrame):
-    print(f"Starting Processing for {symbol}")
-    dpath = download(symbol, catalystBase, timeFrame)
-    process_download(dpath, catalystBase)
-
-def fullDownload(symbolList, catalystBase, timeFrame):
-    """
-    Downloads all symbols in symbolList to catalystBase for timeFrame.
-    """
-    for symbol in symbolList:
-        initializeSymbol(symbol, catalystBase, timeFrame)
-    metadataGenerator.generate_download_metadata(symbolList, timeFrame)
-    metadataGenerator.author_metadata(catalystBase)
-
-def initializeSymbols(symbolList, timeFrame):
-    """
-    Scans symbollist for timeFrame and downloads extra tickers if needed.
-    """
-
-    delta = False
-
-    env_value = os.environ.get('DATA_WAREHOUSE')
+def download_symbols(symbols:list[str],timeframe:TimeFrame, market:Market):
+    env_value = os.environ.get("DATA_WAREHOUSE")
     if not env_value:
         catalystBase = DEFAULTPATH
         print(f"No $DATA_WAREHOUSE found, using default {catalystBase}.")
     else:
         catalystBase = Path(env_value)
-    if not os.path.exists(catalystBase) : os.makedirs(catalystBase)
+    if not os.path.exists(catalystBase):
+        os.makedirs(catalystBase)
     if catalystBase.joinpath("metadata.json").is_file() and os.path.getsize(catalystBase.joinpath("metadata.json")) != 0: #md exists
         print("Metadata Found.")
         metadataGenerator.read_metadata(catalystBase)
-        if metadataGenerator.metadata[metadataGenerator.TIMEFRAME_KEY] >= timeFrame.value:
+        if metadataGenerator.metadata[metadataGenerator.TIMEFRAME_KEY] == timeframe.value:
             print("Timeframe is usable. Performing Optimized Downloads.")
-            for symbol in symbolList:
-                if symbol not in metadataGenerator.symbols:
-                    initializeSymbol(symbol, catalystBase, timeFrame)
-                    metadataGenerator.metadata.add(symbol)
-                    delta = True
-        else:
-            print("Extending Timeframe. Performing Naive Download.")
-            fullDownload(symbolList=symbolList, catalystBase=catalystBase, timeFrame=timeFrame)
-    else: #metadata does not exist
-        print("No Metadata Found. Falling back to naive download.")
-        fullDownload(symbolList=symbolList, catalystBase=catalystBase, timeFrame=timeFrame)
-
-    if delta: #if we have md but need to add a symbol: remove old md, write new.
-        print("Writing Metadata")
-        metadataGenerator.delete_metadata()
-        metadataGenerator.author_metadata(catalystBase)
-    print("Downloads Complete!")
-
+            for symbol in metadataGenerator.find_uncached_symbols(symbols, market):
+                downloaded_path = download(symbol, catalystBase, timeframe, market)
+                process_download(downloaded_path, catalystBase)
+                metadataGenerator.on_download(symbol=symbol, market=market, timeframe=timeframe)
+        metadataGenerator.overwrite_metadata(catalystBase)
+    else: #fresh download
+        print("Performing fresh download of requested symbols.")
+        for symbol in symbols:
+            downloaded_path = download(symbol, catalystBase, timeframe, market)
+            process_download(downloaded_path, catalystBase)
+            metadataGenerator.on_download(symbol=symbol, market=market, timeframe=timeframe)
+        metadataGenerator.store_metadata(catalystBase)
 
 if __name__ == "__main__":
-    tickers = ["BTCUSDT", "ETHUSDT", "BNBUSDT", "SOLUSDT", "XRPUSDT"]
-    tickers = ["XRPUSDT"]
-    initializeSymbols(tickers, TimeFrame.TEST)
+    tickers = ["SOLUSDT", "XRPUSDT", "DOGEUSDT"]
+    market = Market.BINANCE_USDT_FUTURES
+    download_symbols(tickers, TimeFrame.TEST, market)
--- utils/DataFetching/helpers/downloader.py
+++ utils/DataFetching/helpers/downloader.py
@@ -1,19 +1,21 @@
-from tardis_dev import datasets
-from catalyst.constants import _DOWNLOAD_TIMEFRAME_MAP
-from catalyst.secrets import TARDIS_SECRET
-from pathlib import Path
-import nest_asyncio
-import shutil
-import os
-
+from catalyst.constants import _DOWNLOAD_TIMEFRAME_MAP, MARKET_MAP
 from catalyst.utils.utils.DataFetching.metadata.mdtimeframe import TimeFrame
+from catalyst.utils.utils.DataFetching.markets import Market
+from catalyst.secrets import DATAKEYS
 
-def download(symbol, catalystBase, timeFrame):
+from pathlib import Path
+import shutil
+
+import nest_asyncio
+from tardis_dev import datasets
+
+
+def download(symbol:str, catalystBase:Path, timeFrame:TimeFrame, market:Market):
     start, end = _DOWNLOAD_TIMEFRAME_MAP[timeFrame]
     nest_asyncio.apply()
     SYMBOL = symbol
     datasets.download(
-        exchange="binance",
+        exchange=MARKET_MAP[market],
         data_types=[
             "incremental_book_L2",
             "trades",
@@ -22,10 +24,10 @@ def download(symbol, catalystBase, timeFrame):
         from_date=start,
         to_date=end,
         symbols=[SYMBOL],
-        concurrency=os.cpu_count()/2, #beta af man 
-        api_key=TARDIS_SECRET,
+        concurrency=5,
+        api_key=DATAKEYS[market],
     )
-    targetPath = catalystBase.joinpath(SYMBOL[0:(len(SYMBOL)-4)]+"-SPOT")
+    targetPath = catalystBase.joinpath(SYMBOL)
     shutil.copytree(Path.cwd().joinpath("datasets"), targetPath)
     shutil.rmtree(Path.cwd().joinpath("datasets"))
     print(f"Finished downloading for {symbol}.")
